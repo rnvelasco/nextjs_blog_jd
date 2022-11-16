48fad471879dc24195b1774c9e64e735b12b9b58
Robert Velasco <robertvelasco@Roberts-MacBook-Pro.local>
2022-11-16 20:19:27 +0800

    second commit

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Robert Velasco';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'John Rald Website';
 
 export default function Layout({ children, home }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,10 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I am Robert.  I'm a software engineer, scrum master and an educator.</p>
+        <p>Hello, I am John Rald.  I'm a grade 6 student of St. Scholastica Academy</p>
+        <p>My hobbies are playing Roblux, watching youtube videos and playing chess</p>
+        <p>My favorite food are ramen, maki and my lola's sisig</p>
+        <p>Someday I want to be a gasoline boy!</p>        
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
